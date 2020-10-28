a32f1d840901690ad569f89835270143d0b03bd8
Alexei <alexei@Sherab-Zangpos-MacBook-Pro.local>
2020-10-28 14:57:15 +0100

    added features; finished project

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// @ts-check
-
+// gets input values & adds them to a new row at bottom of the table
 function addRowToTable() {
     // get form input values
     let name = document.getElementById("name-field").value;
@@ -33,6 +32,7 @@ function addRowToTable() {
     }
 }
 
+//  creates & appends delete btn to new row
 function addDeleteButton(row) {
     let deleteBtnCell = row.insertCell(-1);
     const deleteButton = document.createElement("input");
@@ -41,9 +41,9 @@ function addDeleteButton(row) {
     deleteButton.setAttribute("onclick", "deleteRow(this)");  
     deleteBtnCell.appendChild(deleteButton);
     row.appendChild(deleteBtnCell);
-    console.log("addDeleteButton() successfully called; result: " + row);
 }
 
+// clears user input from form fields
 function clearFormFields() {
     let formFieldsArr = document.getElementsByClassName("form-control");
     let i;
@@ -52,58 +52,8 @@ function clearFormFields() {
     }
 }
 
-
+// when user clicks a row's delete btn, this function deletes that row
 function deleteRow(r) {
     let i = r.parentNode.parentNode.rowIndex;
     document.getElementById("exp-table").deleteRow(i);
 }
-
-/*function deleteRow(this) {
-    this.parentNode.remove(); //???not sure using 'this' will work??
-    console.log("deleteRow() successfully called; result: (see for yourself - is the target row gone?");
-}
-*/
-/*function getInput() {
-    let inputArr = [];
-    
-
-    
-}
-
-function makeTableFields(inputArr) {
-    let fieldArr = [];
-    let i;
-    for(i=0; i<inputArr.length; i++) {
-        let field = document.createElement("td");
-        let node = document.createTextNode(inputArr[i]);
-        field.appendChild(node);
-        fieldArr.push(field);
-    }
-    console.log("makeTableFields() successfully called; result: " + fieldArr[0]);
-    appendToRow(fieldArr);
-}
-
-// create new table row; fill it with the fields from fieldArr
-function appendToRow(fieldArr) {
-    let newRow = document.createElement("tr");
-    let i;
-    for(i=0; i<fieldArr.length; i++) {
-        newRow.appendChild(fieldArr[i]);
-        console.log("appendToRow() successfully called; result: " + newRow);
-        addDeleteButton(newRow);
-    }
-}
-
-
-
-function addRowToTbl(newRow) {
-    let tbl = document.getElementById("exp-table");
-    let node = document.createTextNode(newRow);
-    tbl.appendChild(node);
-    console.log("addRowToTbl successfully called; result: " + tbl);
-    //clearFormFields();    
-}
-
-
-
-*/
